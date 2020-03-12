23333d6d2c5e02d1834790305a50a3e728b77fd5
Shahjahan Rasel <shadow2burn@gmail.com>
2020-03-12 11:55:10 +0600

    New Activity add for loging and registration

---
+++ app/src/main/java/com/shrasel/nearbydeals/Login.java
@@ -0,0 +1,22 @@
+package com.shrasel.nearbydeals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        this.setTitle("Login");
+    }
+
+    public void go_to_register(View view) {
+        Intent intent = new Intent(getApplicationContext(), Register.class);
+        startActivity(intent);
+    }
+}
